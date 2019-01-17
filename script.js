c95eabd375ea9d91a198d838a29479d17d0aabd4
Toto Corvidoni <totocorvidoni@gmail.com>
2019-01-17 13:15:03 -0300

    Add playRound()

--- script.js
+++ script.js
@@ -8,7 +8,39 @@ function computerPlay() {
       return 'paper'
       break;
     case 3:
-      return 'scissor'
+      return 'scissors'
       break;
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return `It's a tie! Computer also choose ${computerSelection}.`
+  } else {
+    switch (playerSelection) {
+      case 'rock':
+        return (computerSelection == 'scissors') ? win(playerSelection, computerSelection) : lose(playerSelection, computerSelection);
+        break;
+      case 'paper':
+        return (computerSelection == 'rock') ? win(playerSelection, computerSelection) : lose(playerSelection, computerSelection);
+        break;
+      case 'scissors':
+        return (computerSelection == 'paper') ? win(playerSelection, computerSelection) : lose(playerSelection, computerSelection);
+        break;
+      default:
+        return 'Invalid Selection.'
+        break;
+    }
+  }
+}
+
+function win(playerSelection, computerSelection) {
+  return `You Win!! ${playerSelection} beats ${computerSelection}.`
+}
+
+function lose(playerSelection, computerSelection) {
+  return `You lose. ${playerSelection} loses against ${computerSelection}.`
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
